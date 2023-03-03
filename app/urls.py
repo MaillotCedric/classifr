b2af1dff6c7b9f374ea5819293a7c4387b7ecee0
Cédric MAILLOT <cedricmaillot38@gmail.com>
2023-03-03 07:51:19 +0400

    Fix redirection on Chrome

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("images", views.index_images, name="images"),
-    path("predictions", views.index_predictions, name="predictions")
+    path("images/", views.index_images, name="images"),
+    path("predictions/", views.index_predictions, name="predictions")
 ]
