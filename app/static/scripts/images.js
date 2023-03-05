0e622b76c9a53a8ecdd27647a1fe2b9f4f5c22a4
MaillotCedric <cedricmaillot38@gmail.com>
2023-03-05 21:15:01 +0400

    UI / UX - page images     - affichage des images     - mise en place du filtrage par catégorie

--- app/static/scripts/images.js
+++ app/static/scripts/images.js
@@ -1,10 +1,41 @@
-$.ajax({
-    type: "GET",
-    url: "../api/image",
-    success: (data) => {
-        afficher(data);
-    },
-    error: () => {
-        afficher("error");
-    }
+function creer_select_categorie(data) {
+    let categories = data.results;
+    let select_categorie = document.getElementById("select-categorie");
+
+    select_categorie.innerHTML = `
+        <option value="" selected>Sélectionner une catégorie</option>
+    `;
+
+    categories.forEach(categorie => {
+        select_categorie.innerHTML += `
+            <option value="`+ categorie.id +`">`+ categorie.nom +`</option>
+        `;
+    });
+};
+
+function afficher_images(data) {
+    let images = data.results;
+    let images_container = document.getElementById("images-container");
+
+    images_container.innerHTML = "";
+    images.forEach(image => {
+        images_container.innerHTML += `
+            <div class="img-thumbnail">
+                <img src="`+ image.chemin +`" alt="`+ image.nom +`"/>
+            </div>
+        `;
+    });
+};
+
+ajax_call("GET", "../api/categorie", donnees={}, success_callback=creer_select_categorie);
+
+ajax_call("GET", "../api/image", donnees={}, success_callback=afficher_images);
+
+$(document).on("change", "#select-categorie", function(event){
+    event.preventDefault();
+
+    id_categorie = event.target.value;
+    ajax_url = id_categorie !== "" ? "../api/image/?categorie_id="+id_categorie : "../api/image";
+    
+    ajax_call("GET", ajax_url, donnees={}, success_callback=afficher_images, error_callback=afficher_error);
 });
