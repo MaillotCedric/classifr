153aed00f1a8586ef166843c87785c15bbf13443
Cédric MAILLOT <cedricmaillot38@gmail.com>
2023-03-07 08:22:11 +0400

    Update action predict
    - sauvegarde de l'image dans un dossier spécifique

--- app/toolbox.py
+++ app/toolbox.py
@@ -1,8 +1,14 @@
 from PIL import Image
 
+from io import BytesIO
+
 import numpy as np
 
 import os
+
+import re
+
+import base64
 
 def get_image(image_path, shape):
     image = Image.open(image_path).resize(shape)
@@ -21,5 +27,8 @@ def predict_image(model, image_path, shape):
 
     return labels[label_index]
 
-def save_image(image_path, saved_image_path, image_name):
-    Image.open(image_path).save(os.path.join(saved_image_path, image_name))
+def save_image(image_base_64, saved_image_path, image_name):
+    image_data = re.sub("^data:image/.+;base64,", "", image_base_64)
+    image = Image.open(BytesIO(base64.b64decode(image_data)))
+
+    image.save(os.path.join(saved_image_path, image_name))
