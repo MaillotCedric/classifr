816188cc5d15a362039fe45cb39f664a507d33a5
LaurentDIA16 <dia16@polenumerique.re>
2023-03-09 14:57:26 +0400

    Update modeles.js
    Ajout d'une fonction pour afficher par défaut le dernier modèle créé au chargement de la page modèles

--- app/static/scripts/modeles.js
+++ app/static/scripts/modeles.js
@@ -1,3 +1,186 @@
+$(document).ready(function() {
+    ajax_call("GET", "../api/modele", donnees={}, success_callback=afficher_dernier_modele, error_callback=afficher_error);
+});
+
+function afficher_dernier_modele(data) {
+    var dernier_modele = data.results.reverse()[0];;
+    console.log(dernier_modele);
+
+    var modeles_container = document.getElementById("modeles-container");
+
+    // Convertir la date au format "dd-mm-yyyy"
+    let date_created = new Date(dernier_modele.date_created);
+    
+    
+    let day = date_created.getDate().toString().padStart(2, '0');
+    let month = (date_created.getMonth() + 1).toString().padStart(2, '0');
+    let year = date_created.getFullYear();
+    let date_created_formatted = `${day}-${month}-${year}`;
+    console.log(date_created_formatted);
+
+    modeles_container.innerHTML = `
+        <div class="modele-thumbnail">
+        <h1>${dernier_modele.nom}</h1>
+        <h3>Entraînement terminé le  ${date_created_formatted}</h3>
+        <br>
+        
+        <div class="row">
+            <div class="col-md-12 col-lg-5">
+                <p class="gauge-title text-center" style="font-weight: bold;">Précision</p>
+                <canvas id="gauge-chart-precision"></canvas>
+            </div>
+            <div class="col-md-12 col-lg-5">
+                <p class="gauge-title text-center" style="font-weight: bold;">Recall</p>
+                <canvas id="gauge-chart-recall"></canvas>
+            </div>
+        </div>
+
+        <div class="card" style="width: 35rem;">
+            <div class="card-body">
+                <h4 class="card-title">Catégories</h4>
+                <br>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h5 class="card-subtitle mb-2 text-muted">Nom de la catégorie</h5>
+                    </div>
+                <div class="col-md-6">
+                    <h5 class="card-subtitle mb-2 text-muted">Nb d'images</h5>
+                </div>
+                </div>
+                <hr>
+                <div id="categories" class="card-text"></div>
+            </div>
+        </div>
+    `;
+
+
+    // Afficher les jauges
+    var precision_value = dernier_modele.accuracy;
+    var recall_value = dernier_modele.recall;
+
+    var config_precision = {
+        type: 'gauge',
+        data: {
+            labels: ['Fail', 'Warning', 'Warning','Success'],
+            datasets: [{
+                data: [25, 50, 85, 100],
+                value: precision_value,
+                minValue: 0,
+                backgroundColor: ['red','orange','yellow','green'],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            responsive: true,
+            layout: {
+                padding: {
+                    bottom: 30
+                }
+            },
+            min: 0,
+            max: 100,
+            needle: {
+                radiusPercentage: 2,
+                widthPercentage: 3.2,
+                lengthPercentage: 80,
+                color: 'rgba(0, 0, 0, 1)'
+            },
+            valueLabel: {
+                formatter: Math.round,
+                display: true,
+                fontSize: 20,
+                backgroundColor: 'green'
+            },
+            plugins: {
+                datalabels: {
+                    display: true,
+                    formatter:  function (value, context) {
+                        return context.chart.data.labels[context.dataIndex];
+                    },
+                    color: 'rgba(0, 0, 0, 1.0)',
+                    backgroundColor: null,
+                    font: {
+                        size: 15,
+                        weight: 'bold'
+                    },
+                },
+            },
+        },
+    };
+
+    var config_recall = {
+        type: 'gauge',
+        data: {
+            labels: ['Fail', 'Warning', 'Warning','Success'],
+            datasets: [{
+                data: [25, 50, 85, 100],
+                value: recall_value,
+                minValue: 0,
+                backgroundColor: ['red','orange','yellow','green'],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            responsive: true,
+            layout: {
+                padding: {
+                    bottom: 30
+                }
+            },
+            min: 0,
+            max: 100,
+            needle: {
+                radiusPercentage: 2,
+                widthPercentage: 3.2,
+                lengthPercentage: 80,
+                color: 'rgba(0, 0, 0, 1)'
+            },
+            valueLabel: {
+                formatter: Math.round,
+                display: true,
+                fontSize: 20,
+                backgroundColor: 'green'
+            },
+            plugins: {
+                datalabels: {
+                    display: true,
+                    formatter:  function (value, context) {
+                        return context.chart.data.labels[context.dataIndex];
+                    },
+                    color: 'rgba(0, 0, 0, 1.0)',
+                    backgroundColor: null,
+                    font: {
+                        size: 15,
+                        weight: 'bold'
+                    },
+                },
+            },
+        },
+    };
+
+    var ctx = document.getElementById('gauge-chart-precision').getContext('2d');
+    window.myGauge = new Chart(ctx, config_precision);
+
+    var ctx = document.getElementById('gauge-chart-recall').getContext('2d');
+    window.myGauge = new Chart(ctx, config_recall);
+    
+    // Récupérer les détails du modèle
+    let id_modele = dernier_modele.id;
+    let url_details_modele = "../api/details-modele/?modele_id=" + id_modele;
+
+    ajax_call("GET", url_details_modele, donnees={}, success_callback= function(data) {
+        let categories = data.results.filter(function(item) {
+            return item.modele_id === id_modele;
+        }).map(function(item) {
+            return { nom: item.categorie.nom, nb_images: item.nb_images };
+        });
+        let categories_string = categories.map(function(item) {
+            return "<div class='row'><div class='col-md-6'>" + item.nom + "</div><div class='col-md-6'>" + item.nb_images + "</div></div><br>";
+        }).join("");
+        document.getElementById("categories").innerHTML = categories_string;
+        }, error_callback=afficher_error);
+};
+
 function creer_select_modele(data) {
     let modeles = data.results.reverse();
     let select_modele = document.getElementById("select-modele");
