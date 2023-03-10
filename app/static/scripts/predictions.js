fd9f330bb9e965092cd580c293b63f8706c33c4c
MaillotCedric <cedricmaillot38@gmail.com>
2023-03-10 20:02:22 +0400

    UI : details prédiction - ajout de l'image sur laquelle la prédiction à été réalisée

--- app/static/scripts/predictions.js
+++ app/static/scripts/predictions.js
@@ -5,12 +5,12 @@ function afficher_predictions(data) {
     predictions_container.innerHTML = "";
     predictions.forEach(prediction => {
         predictions_container.innerHTML += `
-            <div id_prediction="`+ prediction.id +`" class="img-thumbnail" data-toggle="modal" data-target="#modal-details-prediction">
-                <img src="`+ prediction.image.chemin +`" alt="`+ prediction.image.nom +`"/>
+            <div id_prediction="`+ prediction.id +`" class="img-thumbnail img-thumbnail-preview" data-toggle="modal" data-target="#modal-details-prediction-`+ prediction.id +`">
+                <img id_prediction="`+ prediction.id +`" src="`+ prediction.image.chemin +`" alt="`+ prediction.image.nom +`"/>
             </div>
 
             <!-- modal détails prédiction -->
-            <div class="modal fade" id="modal-details-prediction" tabindex="-1" role="dialog" aria-labelledby="detailsPredictionModalLabel" aria-hidden="true">
+            <div class="modal fade" id="modal-details-prediction-`+ prediction.id +`" tabindex="-1" role="dialog" aria-labelledby="detailsPredictionModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -20,7 +20,13 @@ function afficher_predictions(data) {
                             </button>
                         </div>
                         <div class="modal-body">
-                            ...
+                            <div id="details-prediction-`+ prediction.id +`-image-container" class="img-thumbnail">
+                                image
+                            </div>
+                            <hr/>
+                            <div id="details-prediction-`+ prediction.id +`-resultats-container">
+                                résultats
+                            </div>
                         </div>
                         <!-- <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
@@ -69,6 +75,17 @@ function creer_selects(data) {
     creer_select_modele_filtre(data);
 };
 
+function afficher_image_details_prediction(data) {
+    let id_prediction = data.id;
+    let chemin_image = data.image.chemin;
+    let image_container = document.getElementById("details-prediction-"+ id_prediction +"-image-container");
+
+    image_container.innerHTML = "";
+    image_container.innerHTML = `
+        <img src="`+ chemin_image +`"/>
+    `;
+};
+
 ajax_call("GET", "../api/modele", donnees={}, success_callback=creer_selects, error_callback=afficher_error);
 ajax_call("GET", "../api/prediction", donnees={}, success_callback=afficher_predictions, error_callback=afficher_error);
 
@@ -113,4 +130,13 @@ $(document).on("change", "#select-modele-filtre", function(event){
     ajax_url = id_modele !== "" ? "../api/prediction/?modele_id="+id_modele : "../api/prediction";
     
     ajax_call("GET", ajax_url, donnees={}, success_callback=afficher_predictions, error_callback=afficher_error);
+});
+
+$(document).on("click", ".img-thumbnail-preview", (event) => {
+    id_prediction = event.target.attributes.id_prediction.value;
+    ajax_url_prediction_instance = "../api/prediction/" + id_prediction;
+    ajax_url_prediction_resultats = "../api/resultat/?prediction_id=" + id_prediction;
+
+    ajax_call("GET", ajax_url_prediction_instance, donnees={}, success_callback=afficher_image_details_prediction, error_callback=afficher_error);
+    // ajax_call("GET", ajax_url_prediction_resultats, donnees={}, success_callback=afficher_success, error_callback=afficher_error);
 });
